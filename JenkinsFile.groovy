826efe992c97b5c81b45e6c132c6db65ae33bfb3
zhumingyuan-2019 <zhumingyuan@buaa.edu.cn>
2023-06-05 16:27:10 -0400

    change groovy

--- JenkinsFile.groovy
+++ JenkinsFile.groovy
@@ -3,7 +3,17 @@ pipeline {
     stages {
         stage('Stage 1') {
             steps {
-                echo 'Hello world!'
+                echo 'Hello world -1!'
+            }
+        }
+        stage('Stage 2') {
+            steps {
+                echo 'Hello world -2!'
+            }
+        }
+        stage('Stage 3') {
+            steps {
+                echo 'Hello world -3!'
             }
         }
     }
