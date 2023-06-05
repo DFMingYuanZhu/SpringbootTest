2c15717f9e4e88798db8e0330f3734ecbd5e80cd
zhumingyuan-2019 <zhumingyuan@buaa.edu.cn>
2023-06-05 16:39:48 -0400

    change groovy

--- doc/Jenkinsfile.groovy
+++ doc/Jenkinsfile.groovy
@@ -1,13 +1,6 @@
 retry(3) {
     for (int i = 0; i < 10; i++) {
-        branches["branch${i}"] = {
-            node {
-                retry(3) {
-                    checkout scm
-                }
-                sh 'make world'
-            }
-        }
+        echo "hello world {i}"
     }
 }
 parallel branches
